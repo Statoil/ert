5956eb7e469bb18c2be0fc948ffbfc415c5cb011
Morten Bendiksen <morten.bendiksen@gmail.com>
2021-04-28 13:13:51 +0200

    Move dialogue to narrative_proxy

--- tests/ensemble_evaluator/test_ensemble_evaluator.py
+++ tests/ensemble_evaluator/test_ensemble_evaluator.py
@@ -1,11 +1,6 @@
 import re
-from contextlib import contextmanager
 
-import websockets
 import pytest
-import asyncio
-import threading
-import queue
 
 from ert_shared.ensemble_evaluator.client import Client
 from ert_shared.ensemble_evaluator.entity import serialization
@@ -28,16 +23,9 @@ from tests.narrative import (
     EventDescription,
     Consumer,
     Provider,
-    _Narrative,
-    _Response,
-    _Request,
-    _Interaction,
-    _InteractionTermination,
-    _RecurringResponse,
-    _RecurringRequest,
     ReMatch,
-    InteractionDirection,
 )
+from tests.narrative_proxy import NarrativeProxy
 from tests.narratives import monitor_happy_path_narrative
 
 
@@ -143,77 +131,6 @@ def test_dispatchers_can_connect_and_monitor_can_shut_down_evaluator(evaluator):
                         assert False, "got unexpected event from monitor"
 
 
-class Dialogue(object):
-    def __init__(self, narrative: _Narrative):
-        self.narrative = narrative
-        self.currentInteraction = narrative.interactions.pop()
-        self.error = None
-
-    async def _async_proxy(self, url, q):
-        self.done = asyncio.get_event_loop().create_future()
-
-        async def handle_messages(msg_q: asyncio.Queue, done: asyncio.Future):
-            try:
-                for interaction in self.narrative.interactions:
-                    await interaction.verify(msg_q.get)
-            except Exception as e:
-                done.set_result(e)
-
-        async def handle_server(server, client, msg_q):
-            async for msg in server:
-                await msg_q.put((InteractionDirection.RESPONSE, msg))
-                print(f"from server: {msg}")
-                await client.send(msg)
-
-        async def handle_client(client, _path):
-            msg_q = asyncio.Queue()
-            if _path == "/client":
-                async with websockets.connect(url + _path) as server:
-                    msg_task = asyncio.create_task(handle_messages(msg_q, self.done))
-                    server_task = asyncio.create_task(
-                        handle_server(server, client, msg_q)
-                    )
-
-                    async for msg in client:
-                        print(f"from client: {msg}")
-                        await msg_q.put((InteractionDirection.REQUEST, msg))
-                        await server.send(msg)
-
-                    server_task.cancel()
-                    await server_task
-                    await msg_task
-
-        async with websockets.serve(handle_client, host="localhost", port=0) as s:
-            port = s.sockets[0].getsockname()[1]
-            asyncio.get_event_loop().run_in_executor(None, lambda: q.put(port))
-            asyncio.get_event_loop().run_in_executor(None, lambda: q.put(self.done))
-            error = await self.done
-            print(f"Done. Error: {error}")
-            q.put(error)
-
-    def _proxy(self, url, q):
-        asyncio.set_event_loop(asyncio.new_event_loop())
-        q.put(asyncio.get_event_loop())
-        asyncio.get_event_loop().run_until_complete(self._async_proxy(url, q))
-        print("her")
-
-    @contextmanager
-    def proxy(self, url):
-        q = queue.Queue()
-        t = threading.Thread(target=self._proxy, args=(url, q))
-        t.start()
-        loop = q.get()
-        port = q.get()
-        done = q.get()
-        yield port
-        if not done.done():
-            loop.call_soon_threadsafe(done.set_result, None)
-        t.join()
-        error = q.get()
-        if error:
-            raise error
-
-
 def test_ensemble_monitor_communication_given_success(ee_config, unused_tcp_port):
     ensemble = TestEnsemble(iter=1, reals=2, steps=2, jobs=2)
     ee = EnsembleEvaluator(
@@ -272,7 +189,7 @@ def test_ensemble_monitor_communication_given_success(ee_config, unused_tcp_port
     )
 
     ee.run()
-    with Dialogue(narrative).proxy(ee_config.url) as port:
+    with NarrativeProxy(narrative).proxy(ee_config.url) as port:
         with ee_monitor.create("localhost", port) as monitor:
             for event in monitor.track():
                 if event["type"] == identifiers.EVTYPE_EE_SNAPSHOT:
@@ -375,7 +292,7 @@ def test_ensemble_monitor_communication_given_failing_job(ee_config, unused_tcp_
     )
     with ee.run() as m:
         pass
-    with Dialogue(narrative).proxy(ee_config.url) as port:
+    with NarrativeProxy(narrative).proxy(ee_config.url) as port:
         with ee_monitor.create("localhost", port) as monitor:
             for event in monitor.track():
                 print(f"monitor received: {event}")
@@ -419,7 +336,7 @@ def test_verify_narratives2(ee_config, caplog):
 
     with ee.run() as m:
         pass
-    with Dialogue(monitor_happy_path_narrative).proxy(ee_config.url) as port:
+    with NarrativeProxy(monitor_happy_path_narrative).proxy(ee_config.url) as port:
         with ee_monitor.create("localhost", port) as monitor:
             for event in monitor.track():
                 print(f"monitor received: {event}")
